ee9dead98483b54c09d3aa6d026519799e2a2b5c
unknown <alexm1397@gmail.com>
2021-10-23 11:56:57 -0500

    first question in place, ans obj logged successfully in generate sript, file created with test text

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateREADME = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
+const promptProject = projectData => {
+    console.log(`
+        ======================
+        Generate a new ReadMe!
+        ======================
+    `)
+    if (!projectData) {
+        projectData = [];
+    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is your Project Name? (Required)',
+            validate: projectNameInput => {
+                if(projectNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+        }
+    ]).then(projectData => {
+        //portfolioData.push(projectData);
+        const pageMD = generateREADME(projectData);
+        fs.writeFile('./readMe.md', pageMD, err => {
+            if(err) throw new Error(err);
+            console.log('Page Created! Checkout readme.md in this directory to see it!');
+        })
+    })
+}
+
+            
+
+promptProject()
+//console.log(portfolioData)
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
